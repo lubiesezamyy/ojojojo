c7f9dae148a60fe47f35674599714d149dba0b0b
Martiso <cos@nic.lol>
2018-10-11 17:48:52 +0200

    balalaklakjcbskbc

---
+++ lubieraki.py
@@ -0,0 +1,15 @@
+ileOwiec = input("jak duzo piramide z owiec mam narysowac: ")
+print("przed monwertowaniem to jeset tryb: ", type(ileOwiec))
+ileOwiec = int(ileOwiec)
+#zadanie domowe
+"""napisz program ktory pobierze liczbe uzytkownnika i wyswietli piramide z kotkow 
+lub owiec np. dla liczby 3 i zalozmy kotkow =^.^=
+=^.^=
+=^.^==^.^=
+=^.^==^.^==^.^=
+=^.^==^.^==^.^==^.^=
+hint ify i fory
+zad dodatkowe 1.5pkt 
+zabezpiecz  sie przed errorem ktory wyskkuje gdy  user pisze do inputa napis  a
+nie liczbe
+"""
